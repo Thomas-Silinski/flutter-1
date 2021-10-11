6a6bf08c96984f2f5d2dd09cb287a284c852866e
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2021-10-11 19:11:16 +0200

    Added Article Controller

---
+++ lib/controllers/article_controller.dart
@@ -0,0 +1,45 @@
+import 'package:get/get.dart';
+import 'package:project/models/article.dart';
+
+import 'controller.dart';
+
+typedef JsonArticle = Map<String, String>;
+typedef JsonArticleList = List<JsonArticle>;
+
+class ArticleController extends AppController {
+  @override
+  String storeId = 'articles';
+
+  final List<Article> articles = <Article>[].obs;
+  List<dynamic> _jsonList = <dynamic>[];
+
+  @override
+  void onInit() {
+    _restoreArticles();
+    super.onInit();
+  }
+
+  void addAndStoreArticle(Article article) {
+    articles.add(article);
+
+    _jsonList.add(<String, String>{
+      'title': article.title,
+      'content': article.content,
+      'id': article.id,
+    });
+    storage = _jsonList;
+  }
+
+  /// Populates the controller from the local storage
+  void _restoreArticles() {
+    _jsonList = storage;
+
+    for (final dynamic article in _jsonList) {
+      articles.add(Article(
+        title: article['title'],
+        content: article['content'],
+        id: article['id'],
+      ));
+    }
+  }
+}
