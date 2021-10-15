84a872b6baae49e5d608062f3b266e5a33bec4e0
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2021-10-15 17:43:47 +0200

    Added author to Article

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -3,23 +3,27 @@ import 'model.dart';
 class Article extends Model {
   String title;
   String content;
+  String author;
 
   Article({
     String? id,
     required this.title,
     required this.content,
+    required this.author,
   }) : super(id: id);
 
   @override
   Article.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         content = json['content'],
+        author = json['author'],
         super.fromJson(json);
 
   @override
   Map<String, dynamic> toJson() => {
         'title': title,
         'content': content,
+        'author': author,
         'id': id,
       };
 }
